d2b60308a2cf936f57beab45ce29ca283f43bb6c
nabarun123 <nabarunjyotiboruah@gmail.com>
2022-04-02 10:01:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+let display = document.getElementById("display");
+let buttons = Array.from(document.getElementsByClassName("button"));
+// console.log(buttons);
+
+let arr = [];
+
+buttons.map((button) => {
+  button.addEventListener("click", (e) => {
+    // console.log(e.target.innerText);
+    switch (e.target.innerText) {
+      case "C":
+        display.innerText = "";
+        arr = [];
+        break;
+      case "=":
+        try {
+          display.innerText = eval(arr.join(""));
+        } catch {
+          display.innerText = "Error";
+        }
+        break;
+      case "<":
+        if (display.innerText) {
+          display.innerText = arr.join("").slice(0, -1);
+          arr.pop();
+        }
+        break;
+      default:
+        display.innerText = e.target.innerText;
+        arr.push(e.target.innerText);
+        console.log(arr);
+        console.log(arr.join(""));
+    }
+  });
+});
